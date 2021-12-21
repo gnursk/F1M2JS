bc68c259f48874d77719ff63d5f6f7517ca139df
Nick <32829@ma-web.nl>
2021-12-21 15:17:51 +0100

    les 2 boodschappen

--- web/les2-boodschappen/script.js
+++ web/les2-boodschappen/script.js
@@ -1,1 +1,22 @@
-console.log("script is geladen!")
+console.log("script is geladen!");
+
+const groentevak = document.getElementById('groente');
+const fruitvak = document.getElementById('fruit');
+
+function ZetBijFruit(fruit){
+    console.log(fruit);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = fruit;
+    nieuwElement.className = 'rood';
+    fruitvak.appendChild(nieuwElement);
+}
+
+function ZetBijGroente(groente){
+    console.log(groente);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = groente;
+    nieuwElement.className = 'groen';
+    groentevak.appendChild(nieuwElement);
+}
